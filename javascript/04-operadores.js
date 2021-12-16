e9db2239d8ce62d274fe65a7f307521f83111bcc
Javier de Jesús Pérez Santiago <javier.de.jesus.perez.santiago@gmail.com>
2021-12-15 19:15:36 -0600

    agregue mis ejercicios

---
+++ javascript/04-operadores.js
@@ -0,0 +1,74 @@
+/*
+
+Operadores aritmeticos: nos sirven para hacer operaciones
++ Sirven para sumar o concatenar, cuando es letras se concatena, numeros los suma
+- Sirve para restar numeros
+* Sirve para multiplicar 2 numeros
+% modulo, nos sirve para obtener el residuo de una division
+/ nos sirve para dividir dos numeros;
+
+*,-,% y / son solo para numeros
+*/
+
+/*
+//con suma +
+let numero1 = 34;
+let numero2 = 5;
+
+console.log(numero1 + numero2); 
+*/
+
+
+/*
+
+const numero1 = 34;
+const numero2 = 5;
+const suma = numero1 + numero2;
+
+console.log(suma); 
+*/
+
+
+/*
+const numero1 = prompt('Ingresa el numero 1')
+const numero2 = prompt('Ingresa el numero 2');
+const suma = numero1 - numero2;
+
+//console.log(suma);
+
+//console.log(suma);
+//console.log(suma);
+
+*/
+
+
+/*
+
+
+const numero1 = parseInt(prompt('Ingresa el numero 1'));
+const numero2 = parseInt(prompt('Ingresa el numero 2'));
+const suma = numero1 + numero2;
+console.log(suma);
+*/
+
+
+//Con /
+
+const numero1 = parseInt(prompt('Ingresa el numero 1'));
+const numero2 = parseInt(prompt('Ingresa el numero 2'));
+
+const suma = numero1 + numero2;
+const resta = numero1 - numero2;
+const multiplicacion = numero1 * numero2;
+const division = numero1 / numero2;
+
+console.log("el numero 1 es: " + numero1);
+console.log("el numero 2 es: " + numero2);
+
+console.log(suma);
+console.log(resta);
+console.log(multiplicacion);
+console.log(division);
+
+console.log("el porcentaje es " + 5 % 3);
+
---
+++ tipos de datos/ejercicios/cuentamedeti.js
@@ -0,0 +1,8 @@
+let nombre = "Javier";
+console.log("mi nombre es: " + nombre);
+let numerofavorito = 7;
+console.log("mi numero favorito es el: " + numerofavorito);
+let mascota = true;
+console.log("tengo mascotas? " + mascota);
+let mascota2 = "si";
+console.log("tengo mascotas? " + mascota2);
