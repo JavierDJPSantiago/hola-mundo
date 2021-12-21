1d9ea92ff6ef46eb6b4a19f7d2ff50512deb833d
Javier de Jesús Pérez Santiago <javier.de.jesus.perez.santiago@gmail.com>
2021-12-21 17:34:21 -0600

    master

---
+++ javascript/javascript/02-let-var.js
@@ -0,0 +1,25 @@
+
+var numero = 40;
+console.log(numero);
+
+if(true) {
+    var numero = 50;
+    console.log(numero); 
+}
+
+console.log(numero);
+
+
+
+
+//que pasa si en vez de var tenemos let?
+
+var texto = 'Bootcamp Java';
+console.log(texto);
+
+if(true){
+    let texto = 'Cohorte 8';
+    console.log(texto);
+}
+
+console.log(texto);
